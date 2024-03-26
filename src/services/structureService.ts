d80f2c0339cd187ce44648e8b8d8a5ad518c85a8
5mk1 <no@email.cc>
2024-03-26 20:27:08 +0500

    chore: structure service refactoring

--- src/services/structureService.ts
+++ src/services/structureService.ts
@@ -1,42 +1,54 @@
-import {BehaviorSubject, Observable} from "rxjs";
+import {BehaviorSubject, map, Observable} from "rxjs";
 import {DescribedNode} from "./describedNode";
 
+class NodesStructure {
+    private _nodesMap: Map<HTMLElement, DescribedNode>;
+    rootNodes: DescribedNode[];
+
+    constructor() {
+        this._nodesMap = new Map<HTMLElement, DescribedNode>();
+        this.rootNodes = [];
+    }
+
+    public add(node: DescribedNode, target: HTMLElement | undefined = undefined): NodesStructure {
+        if (!target) {
+            this.rootNodes.push(node);
+            this._nodesMap.set(node.node, node);
+            return this;
+        }
+        const foundTarget = this._nodesMap.get(target);
+        if (!foundTarget) {
+            throw new Error('Target element was not found');
+        }
+        foundTarget.children.push(node);
+        this._nodesMap.set(node.node, node);
+        return this;
+    }
+
+    public find(nativeNode: HTMLElement) : DescribedNode | undefined {
+        return this._nodesMap.get(nativeNode);
+    }
+}
+
 export class StructureService {
-    private _nodes: BehaviorSubject<DescribedNode[]>;
+    private _nodes: BehaviorSubject<NodesStructure>;
     public nodes$: Observable<DescribedNode[]>;
 
     constructor() {
-        this._nodes = new BehaviorSubject<DescribedNode[]>([]);
-        this.nodes$ = this._nodes.asObservable();
+        this._nodes = new BehaviorSubject<NodesStructure>(new NodesStructure());
+        this.nodes$ = this._nodes
+            .asObservable()
+            .pipe(map(x => [...x.rootNodes]));
     }
 
     push(node: DescribedNode) {
-        const x = this._nodes.getValue();
-        x.push(node);
-        this._nodes.next([...x]);
+        const structure = this._nodes.getValue().add(node);
+        this._nodes.next(structure); // bug?
     }
 
     appendTo(target: HTMLElement, node: DescribedNode) {
-        const storedNodes = this._nodes.getValue();
-        const storedTarget = this.searchTarget(storedNodes, target);
-        if (!storedTarget) {
-            throw new Error('Target element was not found');
-        }
-        storedTarget.children.push(node);
-        this._nodes.next([...storedNodes]);
-    }
-
-    private searchTarget(searchIn: DescribedNode[], searchFor: HTMLElement): DescribedNode | undefined {
-        for (let node of searchIn) {
-            if (node.node === searchFor) {
-                return node;
-            }
-            const foundInChildren = this.searchTarget(node.children, searchFor);
-            if (foundInChildren) {
-                return foundInChildren;
-            }
-        }
-        return undefined;
+        const structure = this._nodes.getValue().add(node, target);
+        this._nodes.next(structure);
     }
 }
 
