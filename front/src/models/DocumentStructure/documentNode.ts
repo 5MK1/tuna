e6cb12c0726ce3624606e9587d0aa5ab7db59c4d
5MK1 <no@email.cc>
2024-05-20 15:52:54 +0500

    chore: remove getter from DocumentNode

--- front/src/models/DocumentStructure/documentNode.ts
+++ front/src/models/DocumentStructure/documentNode.ts
@@ -1,15 +1,11 @@
 export default class DocumentNode {
     private readonly _node: HTMLElement;
-    private _name: string | undefined;
+    public name: string | undefined;
     public children: DocumentNode[];
     public parent: DocumentNode | undefined;
 
     get nativeNode(): HTMLElement {
         return this._node;
-    }
-
-    get name(): string | undefined {
-        return this._name;
     }
 
     constructor(
@@ -19,7 +15,7 @@ export default class DocumentNode {
         parent: DocumentNode | undefined = undefined
     ) {
         this._node = node;
-        this._name = name;
+        this.name = name;
         this.children = children;
         this.parent = parent;
     }
