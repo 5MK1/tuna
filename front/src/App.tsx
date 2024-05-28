4cd6e16cdd85d033899aaf34c1211ef67fda0809
5MK1 <no@email.cc>
2024-05-28 16:49:20 +0500

    fix: remove unused import

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
 import Toolbox from './components/toolbox/Toolbox';
 import Properties from "./components/properties/Properties";
