23cd15622c17d843db070efda40e3bcb168dd80e
5MK1 <no@email.cc>
2024-05-30 15:49:37 +0500

    fix: move tunaEditor to pages component

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,9 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import './App.scss';
-import Toolbox from './components/toolbox/Toolbox';
-import Properties from "./components/properties/Properties";
-import Board from "./components/board/Board";
-import Cursor from "./components/cursor/Cursor";
-import toolboxContext from "./models/ToolboxContext/toolboxContext";
-import Coordinates from "./models/htmlNativeWrappers/coordinates";
+import TunaEditor from "./pages/tunaEditor";
 
 function App() {
-
-    useEffect(() => {
-        document.body.addEventListener('keydown',({key}:{key: string}) => {
-            if (key.toLocaleLowerCase() === 'escape') {
-                toolboxContext.setTool(undefined);
-            }
-        });
-    }, []);
-
-    return (
-        <div className="app-root"
-             onMouseMove={(e) => {
-                 toolboxContext.setCursorPosition(Coordinates.from(e))
-             }}>
-            <main className="desktop">
-                <Board/>
-            </main>
-            <Toolbox/>
-            <Properties/>
-            <Cursor/>
-        </div>
-    );
+    return (<TunaEditor />);
 }
 
 export default App;
---
+++ front/src/pages/tunaEditor.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect} from "react";
+import toolboxContext from "../models/ToolboxContext/toolboxContext";
+import Coordinates from "../models/htmlNativeWrappers/coordinates";
+import Board from "../components/board/Board";
+import Toolbox from "../components/toolbox/Toolbox";
+import Properties from "../components/properties/Properties";
+import Cursor from "../components/cursor/Cursor";
+
+
+function TunaEditor() {
+    useEffect(() => {
+        document.body.addEventListener('keydown', ({key}: { key: string }) => {
+            if (key.toLocaleLowerCase() === 'escape') {
+                toolboxContext.setTool(undefined);
+            }
+        });
+    }, []);
+
+
+    return (
+        <div className="app-root"
+             onMouseMove={(e) => {
+                 toolboxContext.setCursorPosition(Coordinates.from(e))
+             }}>
+            <main className="desktop">
+                <Board/>
+            </main>
+            <Toolbox/>
+            <Properties/>
+            <Cursor/>
+        </div>
+    );
+}
+
+export default TunaEditor;
