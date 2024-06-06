8702a9957d46db4f758607df03a6df53119285b2
5mk1 <no@email.cc>
2024-06-07 00:17:41 +0500

    fix: app root page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 import './App.scss';
 import AppRoutes from "./pages/routes/appRoutes";
-import {NavLink} from "react-router-dom";
-import routesPaths from "./pages/routes/routesPaths";
 
 function App() {
-    return (<>
-        <div style={{position:'relative'}}>
-            <NavLink to={routesPaths.home}>home</NavLink>
-            <NavLink to={routesPaths.login}>login</NavLink>
-        </div>
-        <AppRoutes />
-    </>);
+    return <AppRoutes />;
 }
 
 export default App;
