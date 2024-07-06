2ed2ac5347f56019fe37a9c13cb9f69f18619c24
5mk1 <no@email.cc>
2024-07-06 14:15:54 +0500

    fix: add CORS, use redis settings from appsettings file

--- server/Server.Api/Program.cs
+++ server/Server.Api/Program.cs
@@ -1,14 +1,26 @@
+using System.Net;
 using Microsoft.OpenApi.Models;
 using Redis.OM;
+using RedLockNet;
+using RedLockNet.SERedis;
+using RedLockNet.SERedis.Configuration;
+using Server.Api.AppSettings;
 using Server.Api.Auth;
 using Tuna.Model.Services.User;
 using Tuna.Repository.InMemory.Users;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 var cfg = builder.Configuration;
 builder.Services.Configure<JwtSettingsOptions>(cfg.GetSection(JwtSettingsOptions.SectionKey));
-
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name:
+		MyAllowSpecificOrigins,
+		policy => { policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().Build(); }
+	);
+});
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,18 +34,22 @@ builder.Services.AddAuth(cfg);
 
 
 // fixme: move to settings
+var redisCfg = RedisSettings.ReadFrom(cfg);
 var redisConnectionProvider = new RedisConnectionProvider(new RedisConnectionConfiguration
 {
-	Host = "redis",
-	Port = 6379
+	Host = redisCfg.Host,
+	Port = redisCfg.Port
 });
 
-// fixme: move to repo
-redisConnectionProvider.Connection.CreateIndex(typeof(UserAccountDocument));
 builder.Services.AddSingleton(redisConnectionProvider);
+var redLockFactory = RedLockFactory.Create(
+	new List<RedLockEndPoint> { new DnsEndPoint(redisCfg.Host, redisCfg.Port) }
+);
+builder.Services.AddSingleton<IDistributedLockFactory>(redLockFactory);
 
 
-builder.Services.AddSwaggerGen(option => {
+builder.Services.AddSwaggerGen(option =>
+{
 	option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
 	option.AddSecurityDefinition("Bearer",
 		new OpenApiSecurityScheme
@@ -56,21 +72,23 @@ builder.Services.AddSwaggerGen(option => {
 					Id = "Bearer"
 				}
 			},
-			new string[] {}
+			new string[] { }
 		}
 	});
 });
 
 var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+	app.UseCors(MyAllowSpecificOrigins);
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
