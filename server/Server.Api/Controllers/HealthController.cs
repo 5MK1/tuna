212eaad7f0c3611b88d8a186ad4fa77bd62852be
5mk1 <no@email.cc>
2024-07-16 02:31:01 +0500

    chore: remove useless logic

--- server/Server.Api/Controllers/HealthController.cs
+++ server/Server.Api/Controllers/HealthController.cs
@@ -24,35 +24,4 @@ public class HealthController : Controller
 		_logger.Log(LogLevel.Information, "health check");
 		return Content("healthy");
 	}
-
-	[HttpGet("set-cookie")]
-	public IActionResult SetCookie()
-	{
-		var expires = DateTime.UtcNow.AddDays(2);
-		var jwt = new JwtSecurityToken(
-			issuer: "https://localhost",
-			audience: "https://localhost",
-			claims: new List<Claim>
-			{
-				new(ClaimTypes.Name, "pumpkin"),
-				new(ClaimTypes.Sid, Guid.Empty.ToString())
-			},
-			expires,
-			signingCredentials: new SigningCredentials(
-				new SymmetricSecurityKey(Encoding.UTF8.GetBytes("MustMustMustBeStoredInSecretStorage69$$")),
-				algorithm: SecurityAlgorithms.HmacSha256
-			)
-		);
-		var tokenString = new JwtSecurityTokenHandler().WriteToken(jwt);
-
-		HttpContext.Response.WriteAccessToken(tokenString, expires);
-		return Ok();
-	}
-
-	[HttpGet("read-cookie")]
-	public ContentResult ReadCookie()
-	{
-		var cookieShit = Request.TryReadAccessToken();
-		return Content(cookieShit ?? "can not read");
-	}
 }
