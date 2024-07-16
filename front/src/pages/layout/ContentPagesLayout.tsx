e2a114e1674103f1f996aac565697ee5d96b3ca0
5mk1 <no@email.cc>
2024-07-16 15:15:56 +0500

    chore: extract component refactoring

--- front/src/pages/layout/ContentPagesLayout.tsx
+++ front/src/pages/layout/ContentPagesLayout.tsx
@@ -1,38 +1,13 @@
 import {Outlet} from "react-router-dom";
-import routesPaths from "../routes/routesPaths";
 import React from "react";
-import "./top-navigation.scss";
 import "./content-pages.scss";
-import TopNavigationLink from "./TopNavigationLink";
 import {observer} from "mobx-react-lite";
-import {userSession} from "../../models/users/userSession";
-import {MouseEvent} from "react/index";
+import ContentPagesNavigation from "./ContentPagesNavigation";
 
 const ContentPagesLayout = observer(({className}: {className?: string | undefined}) => {
-    function getClassName(blockCssClass: string = 'app-root') {
-        return className === undefined
-            ? blockCssClass
-            : `${blockCssClass} ${className}`;
-    }
-
-    async function logout(e: MouseEvent<any>) {
-        e.preventDefault();
-        await userSession.unAuth();
-    }
-
     return (
-        <div className={getClassName()}>
-            <div className="top-navigation">
-                <nav className="top-navigation__items">
-                    <TopNavigationLink to={routesPaths.home} tid="tn__home-link">home</TopNavigationLink>
-                    {userSession.authenticated
-                        && <TopNavigationLink to={routesPaths.editor} tid="tn__editor-link">editor</TopNavigationLink>}
-                    {!userSession.authenticated
-                        && <TopNavigationLink to={routesPaths.login} tid="tn__login-link">login</TopNavigationLink>}
-                    {userSession.authenticated
-                        && <a href="#" className="top-navigation__link" onClick={logout}>logout</a> }
-                </nav>
-            </div>
+        <div className={`app-root ${className}`}>
+            <ContentPagesNavigation />
             <main className="app-main">
                 <Outlet />
             </main>
---
+++ front/src/pages/layout/ContentPagesNavigation.tsx
@@ -0,0 +1,33 @@
+import "./top-navigation.scss";
+import {observer} from "mobx-react-lite";
+import TopNavigationLink from "./TopNavigationLink";
+import routesPaths from "../routes/routesPaths";
+import {userSession} from "../../models/users/userSession";
+import React from "react";
+import {MouseEvent} from "react/index";
+
+const ContentPagesNavigation = observer(() => {
+    async function logout(e: MouseEvent<any>) {
+        e.preventDefault();
+        await userSession.unAuth();
+    }
+
+    return (
+        <div className="top-navigation">
+            <nav className="top-navigation__items">
+                <TopNavigationLink to={routesPaths.home} tid="tn__home-link">home</TopNavigationLink>
+                {userSession.authenticated
+                    && <TopNavigationLink to={routesPaths.editor} tid="tn__editor-link">editor</TopNavigationLink>}
+                {!userSession.authenticated
+                    && <TopNavigationLink to={routesPaths.login} tid="tn__login-link">login</TopNavigationLink>}
+                {userSession.authenticated
+                    && <a href="#"
+                          onClick={logout}
+                          className="top-navigation__link"
+                          data-tid="tn__logout-link">logout</a>}
+            </nav>
+        </div>
+    );
+});
+
+export default ContentPagesNavigation;
